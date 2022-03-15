ae70c295e4f450c6579c5e66f755116d672186e2
MackiewiczT26 <mackiewiczt26@gmail.com>
2022-03-15 22:17:04 +0100

    [12][BE] Copy Paste CRUD Implementation

--- backend/copycasket/models.py
+++ backend/copycasket/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 
 COPY_TYPE_CHOICES = [
-    ('jk', 'Joke'),
-    ('cd', 'Code'),
-    ('in', 'Information'),
-    ('as', 'ASCII'),
-    ('us', 'Unspecified'),
+    ("jk", "Joke"),
+    ("cd", "Code"),
+    ("in", "Information"),
+    ("as", "ASCII"),
+    ("us", "Unspecified"),
 ]
 
 
 class CopyCasket(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=30, blank=False)
-    author = models.CharField(max_length=30, blank=False)  # TODO Implement custom users and make this FK
+    title = models.CharField(max_length=100, blank=False)
+    author = models.CharField(
+        max_length=30, blank=False
+    )  # TODO Implement custom users and make this FK
     creation_date = models.DateTimeField(auto_now_add=True)
-    type = models.CharField(max_length=2, default='us', choices=COPY_TYPE_CHOICES)
+    type = models.CharField(
+        max_length=2,
+        default="us",
+        choices=COPY_TYPE_CHOICES,
+    )
     content = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
+
+    def update(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+        self.save()
