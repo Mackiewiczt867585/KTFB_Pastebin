bbef839cf4984d4eb9b2efb42de621ae1102d903
MackiewiczT26 <mackiewiczt26@gmail.com>
2022-03-23 00:26:09 +0100

    [13][BE]CRUD dla użytkowników
    User CRUD implementation based on Graphene Schema

--- backend/copycasket/schema.py
+++ backend/copycasket/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import CopyCasket
+from .models import CopyCasket, CustomUser
 
 
 class CopyCasketTypes(DjangoObjectType):
@@ -10,15 +10,30 @@ class CopyCasketTypes(DjangoObjectType):
         fields = ("id", "title", "author", "creation_date", "type", "content")
 
 
+class CustomUserTypes(DjangoObjectType):
+    class Meta:
+        model = CustomUser
+        fields = ("email", "username", "first_name", "creation_date", "organisation")
+
+
 class Query(graphene.ObjectType):
     all_copies = graphene.List(CopyCasketTypes)
     copy = graphene.Field(CopyCasketTypes, copy_id=graphene.ID())
+
+    all_users = graphene.List(CustomUserTypes)
+    user = graphene.Field(CustomUserTypes, user_id=graphene.ID())
 
     def resolve_all_copies(self, info):
         return CopyCasket.objects.all()
 
     def resolve_copy(self, info, copy_id):
         return CopyCasket.objects.get(pk=copy_id)
+
+    def resolve_all_users(self, info):
+        return CustomUser.objects.all()
+
+    def resolve_user(self, info, user_id):
+        return CustomUser.objects.get(pk=user_id)
 
 
 class CopyCasketUpdateMutation(graphene.Mutation):
@@ -65,10 +80,61 @@ class CopyCasketCreateMutation(graphene.Mutation):
         return CopyCasketUpdateMutation(copycasket=instance)
 
 
+class CustomUserCreateMutation(graphene.Mutation):
+    class Arguments:
+        email = graphene.String(required=True)
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        first_name = graphene.String()
+        organisation = graphene.String()
+
+    user = graphene.Field(CustomUserTypes)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        instance = CustomUser.objects.create(**kwargs)
+        instance.save()
+        return CustomUserCreateMutation(user=instance)
+
+
+class CustomUserUpdateMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        email = graphene.String(required=False)
+        username = graphene.String(required=False)
+        first_name = graphene.String(required=False)
+        organisation = graphene.String(required=False)
+
+    user = graphene.Field(CustomUserTypes)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        instance = CustomUser.objects.get(pk=id)
+        instance.update(**kwargs)
+        return CustomUserCreateMutation(user=instance)
+
+
+class CustomUserDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    user = graphene.Field(CustomUserTypes)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        instance = CustomUser.objects.get(pk=id)
+        instance.delete()
+        return CustomUserCreateMutation(user=instance)
+
+
 class Mutation(graphene.ObjectType):
     update_copy = CopyCasketUpdateMutation.Field()
     delete_copy = CopyCasketDeleteMutation.Field()
     create_copy = CopyCasketCreateMutation.Field()
+
+    create_user = CustomUserCreateMutation.Field()
+    update_user = CustomUserUpdateMutation.Field()
+    delete_user = CustomUserDeleteMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
