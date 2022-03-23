736db58e28d80200a6cc5b63598d63b504482189
Krzysztof Chacinski <krzysztof.chacinski01@gmail.com>
2022-03-23 23:07:53 +0100

    Cors origin whitelist fix

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -139,7 +139,8 @@ GRAPHENE = {"SCHEMA": "app.schema.schema"}
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000'
 ]
 
 CSRF_TRUSTED_ORIGINS = ['http://0.0.0.0:5432']
