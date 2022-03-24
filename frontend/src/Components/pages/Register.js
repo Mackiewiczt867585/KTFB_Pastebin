4eb79dbec4238c8dc301c21b0dcbb8a6dc895ff5
Filip <filipgcs@gmail.com>
2022-03-24 03:07:41 +0100

    add register user form

---
+++ frontend/src/Components/pages/Register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import Register from '../Register';
+
+
+function Register() {
+  return (
+    <>
+      <h1 className='title'>Rejestracja</h1>
+      <Register />
+    </>
+  );
+}
+
+export default Register;
