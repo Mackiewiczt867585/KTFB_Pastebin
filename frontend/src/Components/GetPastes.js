727ca055014269358dcd72ba6d5e130c56e60d5d
Filip <filipgcs@gmail.com>
2022-03-30 18:03:33 +0200

    [FE] create pagination

--- frontend/src/Components/GetPastes.js
+++ frontend/src/Components/GetPastes.js
@@ -1,3 +1,4 @@
+
 import React, {useEffect, useState} from 'react'
 import { useQuery, gql } from '@apollo/client'
 import {LOAD_PASTES} from '../GraphQL/Queries'
@@ -6,26 +7,14 @@ import TimeAgo from 'javascript-time-ago'
 
 import en from 'javascript-time-ago/locale/en.json'
 import ReactTimeAgo from 'react-time-ago'
+import ReactPaginate from 'react-paginate'
 TimeAgo.addDefaultLocale(en)
 
 
-function GetPastes(){
 
-    const {error, loading, data} = useQuery(LOAD_PASTES);
-    const [ pastes, setPastes ] = useState([]);
-    useEffect(() => {
-        console.log('loading:', loading)
-        console.log('Error:', error)
-        console.log('Pastes:', data)
-        if (data){
-        setPastes(data.allCopies);
-        }
-    }, [data, loading, error]);
+function GetPastes({currentItems}){
 
-
-
-    if (loading) return <p>Loading...</p>;
-    if (data) return(
+ if (currentItems)return(
         <div className='table-box'>
         <table>
             <thead>
@@ -36,8 +25,9 @@ function GetPastes(){
                     <th>Dodano</th>
                 </tr>
             </thead>
+            
             <tbody>
-        {pastes.map((val, pos) => {
+        {currentItems.map((val, pos) => {
             return (
                 <tr key={pos}>
                     <td> {val.title}</td>
@@ -52,4 +42,60 @@ function GetPastes(){
 }
 
 
-export default GetPastes;
+
+function PaginatedItems() {
+  const {error, loading, data} = useQuery(LOAD_PASTES);
+  console.log (data)
+  const [pastes, setPastes] = useState([]);
+  const itemsPerPage=10;
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+
+  useEffect(() => {
+    if(data){
+    setPastes(data.allCopies)
+ 
+    const endOffset = itemOffset + itemsPerPage;
+  
+    setCurrentItems(pastes.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(pastes.length / itemsPerPage));
+  }
+  }, [data,loading,error,pastes,itemOffset]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % pastes.length;
+    setItemOffset(newOffset);
+    };
+    if (loading) return <h1>loading</h1>
+    console.log(currentItems)
+    console.log(pastes)
+  if (currentItems) return (
+    <>
+      <GetPastes currentItems={currentItems} />
+      <div className='page-select'>
+
+      <ReactPaginate
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        />
+        </div>
+    </>
+  );
+}
+
+export default PaginatedItems;
