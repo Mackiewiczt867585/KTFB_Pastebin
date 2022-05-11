2e5244f00f145aaaee2c2ef5bb2b8801172fa3ec
piere01 <piere01@op.pl>
2022-05-11 22:47:40 +0200

    [TEST] tests.py update (resolve private, public and users copies)

--- backend/copycasket/tests.py
+++ backend/copycasket/tests.py
@@ -1,8 +1,8 @@
-"""
 import datetime
 
 from django.test import TestCase
 from django.utils.timezone import make_aware
+from graphql_relay import to_global_id
 
 from .models import CopyCasket, CustomUser
 from .schema import (
@@ -12,87 +12,147 @@ from .schema import (
     CustomUserCreateMutation,
     CustomUserDeleteMutation,
     CustomUserUpdateMutation,
+
     Query,
 )
+
 
 
 class CustomTestCase(TestCase):
     def setUp(self):
         naive_datetime = datetime.datetime.now()
         self.aware_datetime = make_aware(naive_datetime)
-        CopyCasket.objects.create(
-            id=1,
-            title="testowe_dobre",
-            author="zawis",
-            creation_date=self.aware_datetime,
-            type="jk",
-            content="lorem ipsum",
-        )
-        CopyCasket.objects.create(
-            id=2,
-            title="testowe_zle",
-            author="zawis",
-            creation_date=self.aware_datetime,
-            type="jk",
-            content="lorem ipsum",
-        )
-        CustomUser.objects.create(
+        kowal= CustomUser.objects.create(
             id=1,
             email="kowalski@op.pl",
             username="kowal",
             first_name="adam",
-            creation_date=self.aware_datetime,
-            organisation="szwalnia",
+            organisation="org1",
         )
-        CustomUser.objects.create(
+        nadol= CustomUser.objects.create(
             id=2,
             email="nadolny@op.pl",
             username="nadol",
             first_name="pawel",
-            creation_date=self.aware_datetime,
-            organisation="pralnia",
+            organisation="org2",
         )
+        CopyCasket.objects.create(
+            id=1,
+            title="testowe_1",
+            author="nadol",
+            type="jk",
+            content="lorem ipsum",
+            private=False,
+            creator=nadol
+        )
+        CopyCasket.objects.create(
+            id=2,
+            title="testowe_2",
+            author=2,
+            type="jk",
+            content="lorem ipsum",
+            private=False,
+            creator=kowal
+        )
+        CopyCasket.objects.create(
+            id=3,
+            title="testowe_3",
+            author="nadol",
+            type="as",
+            content="lorem ipsum",
+            private=True,
+            creator=kowal
+        )
+        CopyCasket.objects.create(
+            id=4,
+            title="testowe_4",
+            author="kowal",
+            type="us",
+            content="lorem ipsum",
+            private=True,
+            creator=kowal
+        )
+        CopyCasket.objects.create(
+            id=5,
+            title="testowe_5",
+            author="kowal",
+            type="us",
+            content="lorem ipsum",
+            private=True,
+            creator=nadol
+        )
+
 
     def test_query_CopyCasket(self):
         query = Query()
         copyx = CopyCasket.objects.get(id=1)
         copyy = CopyCasket.objects.get(id=2)
-        self.assertEqual(copyx, query.resolve_copy(self, 1))
-        self.assertEqual(copyy, query.resolve_copy(self, 2))
+        self.assertEqual(copyx, query.resolve_copy(self, to_global_id("id", 1)))
+        self.assertEqual(copyy, query.resolve_copy(self, to_global_id("id", 2)))
 
     def test_query_User(self):
         query = Query()
         userx = CustomUser.objects.get(email="kowalski@op.pl")
+        usery = CustomUser.objects.get(email="nadolny@op.pl")
         self.assertEqual(userx, query.resolve_user(self, 1))
+        self.assertEqual(usery, query.resolve_user(self, 2))
 
     def test_query_all(self):
         query = Query()
-        self.assertEqual(2, len(query.resolve_all_copies(self)))
+        self.assertEqual(5, len(query.resolve_all_copies(self)))
 
     def test_users_all(self):
         query = Query()
         self.assertEqual(2, len(query.resolve_all_users(self)))
 
+    def test_resolve_all_public_copies(self):
+        query = Query()
+        self.assertEqual(2, len(query.resolve_all_public_copies(self)))
+
+    def test_resolve_all_private_copies(self):
+        query = Query()
+        copies = query.resolve_all_private_copies(self, creator=2)
+        copies2 = query.resolve_all_private_copies(self, creator=1)
+        self.assertEqual(1, len(copies))
+        self.assertEqual(2, len(copies2))
+
+    def test_resolve_all_users_copies(self):
+        query = Query()
+        copies = query.resolve_all_users_copies(self, creator=2)
+        copies2 = query.resolve_all_users_copies(self, creator=1)
+        self.assertEqual(2, len(copies))
+        self.assertEqual(3, len(copies2))
+
+    def test_resolve_user_email(self):
+        query = Query()
+        userx = CustomUser.objects.get(email="kowalski@op.pl")
+        usery = CustomUser.objects.get(email="nadolny@op.pl")
+        self.assertEqual(userx, query.resolve_user_email(self, "kowalski@op.pl"))
+        self.assertEqual(usery, query.resolve_user_email(self, "nadolny@op.pl"))
+
     def test_createcasket_mut(self):
+        query = Query()
         createq = CopyCasketCreateMutation()
         createq.mutate(
             self,
             None,
-            id=3,
-            title="trzecia_strona",
+            id=6,
+            title="createtest",
             author="zawiso",
             creation_date=self.aware_datetime,
             type="cd",
             content="lorem ipsum",
+            creator="kowalski@op.pl"
         )
-        self.assertEqual("trzecia_strona", CopyCasket.objects.get(id=3).title)
+        self.assertEqual("createtest", CopyCasket.objects.get(id=6).title)
+        self.assertEqual(6, len(query.resolve_all_copies(self)))
 
     def test_updatecasket_mut(self):
         updateq = CopyCasketUpdateMutation()
         updateq.mutate(
             self,
             None,
-            id=2,
+            id=to_global_id("id", 2),
             title="nowe_testowe",
             author="zawis",
             creation_date=self.aware_datetime,
@@ -104,10 +164,13 @@ class CustomTestCase(TestCase):
     def test_deletecasket_mut(self):
         query = Query()
         delq = CopyCasketDeleteMutation()
-        delq.mutate(self, None, id=2)
-        self.assertEqual(1, len(query.resolve_all_copies(self)))
+        delq.mutate(self, None, id=to_global_id("id", 2))
+        self.assertEqual(4, len(query.resolve_all_copies(self)))
+        delq.mutate(self, None, id=to_global_id("id", 3))
+        self.assertEqual(3, len(query.resolve_all_copies(self)))
 
     def test_createuser_mut(self):
+        query = Query()
         createu = CustomUserCreateMutation()
         createu.mutate(
             self,
@@ -120,6 +183,7 @@ class CustomTestCase(TestCase):
             organisation="brak",
         )
         self.assertEqual("brak", CustomUser.objects.get(id=3).organisation)
+        self.assertEqual(3, len(query.resolve_all_users(self)))
 
     def test_updateuser_mut(self):
         updateu = CustomUserUpdateMutation()
@@ -143,4 +207,3 @@ class CustomTestCase(TestCase):
         delu = CustomUserDeleteMutation()
         delu.mutate(self, None, id=2)
         self.assertEqual(1, len(query.resolve_all_users(self)))
-"""
