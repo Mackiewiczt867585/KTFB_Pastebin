b585d9d1f053b87e1ff4b6725ae44480b5ba67e6
Filip <filipgcs@gmail.com>
2022-06-08 00:43:55 +0200

    deleted duplicate

--- backend/copycasket/models.py
+++ backend/copycasket/models.py
@@ -165,16 +165,3 @@ class UserReport(models.Model):
         choices=REPORT_TYPE_CHOICES,
     )
     note = models.TextField(blank=True, null=True)
-
-
-
-
-class UserReport(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False, default=1, related_name="userReport")
-    reason = models.CharField(
-        max_length=2,
-        default="us",
-        choices=REPORT_TYPE_CHOICES,
-    )
-    note = models.TextField(blank=True, null=True)
