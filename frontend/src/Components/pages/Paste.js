3c4b9d2c7686016d2d8ca299edb162559a41cced
Filip <filipgcs@gmail.com>
2022-06-07 04:48:20 +0200

    added creator

--- frontend/src/Components/pages/Paste.js
+++ frontend/src/Components/pages/Paste.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import "../../App.css";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { PASTE_BY_ID } from "../../GraphQL/Queries";
 import { useQuery, gql } from "@apollo/client";
 import { Form, Button, Icon} from 'semantic-ui-react';
@@ -20,24 +20,46 @@ function Paste() {
     } else {
     }
   }, [data, loading, error]);
-  console.log(pastes.images)
- 
+  console.log(pastes)
+
+
+
+const creator = pastes.creator ? (
+  <p>creator: <Link to = {'/user/' + pastes.creator.email}>{pastes.creator.username}</Link></p>
+) : (
+  <p>creator: Guest</p>
+);
+
+
+
+
+
+
+
+
+
+
   return (
-    <>
+    <div>
       <h1 className="title">Wklejka</h1>
       <div className="outer-box">
-        <div>
+        <div className="flex-display">
           <p>author: {pastes.author}</p>
-          <h1>content:</h1>
+          {creator}
         </div>
+          <h1 className="wklejka">content:</h1>
+        <div className="content">
         <p>{pastes.content}</p>
-          <p>{pastes.image}</p>
         <img src={pastes.image}>
         </img>
-        {/* <button onClick={navigator.clipboard.writeText(window.location)}>Get Link</button> */}
         </div>
-    </>
-  );
+        <div className="flex-display padding-bottom">
+        <button className="paste-link-btn" onClick={navigator.clipboard.writeText(window.location)}>Skopiuj link do wklejki</button>
+        <button className="paste-link-btn"><Link className="report-link" to={'/paste/' + pastes.id + "/report"}>Report</Link></button>
+        </div>
+        </div>
+        </div>
+          )
 }
 
 export default Paste;
